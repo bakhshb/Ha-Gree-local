a5dd22869bb406b687e083702c428bb528f2c41c
bakhshb <bakhshb@gmail.com>
2024-11-02 03:23:29 +0300

    Update coordinator.py

--- custom_components/gree/coordinator.py
+++ custom_components/gree/coordinator.py
@@ -105,45 +105,45 @@ class DeviceDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             )
 
 
-# class DiscoveryService(Listener):
-#     """Discovery event handler for gree devices."""
+class DiscoveryService(Listener):
+    """Discovery event handler for gree devices."""
 
-#     def __init__(self, hass: HomeAssistant) -> None:
-#         """Initialize discovery service."""
-#         super().__init__()
-#         self.hass = hass
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize discovery service."""
+        super().__init__()
+        self.hass = hass
 
-#         self.discovery = Discovery(DISCOVERY_TIMEOUT)
-#         self.discovery.add_listener(self)
+        self.discovery = Discovery(DISCOVERY_TIMEOUT)
+        self.discovery.add_listener(self)
 
-#         hass.data[DOMAIN].setdefault(COORDINATORS, [])
+        hass.data[DOMAIN].setdefault(COORDINATORS, [])
 
-#     async def device_found(self, device_info: DeviceInfo) -> None:
-#         """Handle new device found on the network."""
+    async def device_found(self, device_info: DeviceInfo) -> None:
+        """Handle new device found on the network."""
 
-#         device = Device(device_info)
-#         try:
-#             await device.bind()
-#         except DeviceNotBoundError:
-#             _LOGGER.error("Unable to bind to gree device: %s", device_info)
-#         except DeviceTimeoutError:
-#             _LOGGER.error("Timeout trying to bind to gree device: %s", device_info)
+        device = Device(device_info)
+        try:
+            await device.bind()
+        except DeviceNotBoundError:
+            _LOGGER.error("Unable to bind to gree device: %s", device_info)
+        except DeviceTimeoutError:
+            _LOGGER.error("Timeout trying to bind to gree device: %s", device_info)
 
-#         _LOGGER.debug(
-#             "Adding Gree device %s at %s:%i",
-#             device.device_info.name,
-#             device.device_info.ip,
-#             device.device_info.port,
-#         )
-#         coordo = DeviceDataUpdateCoordinator(self.hass, device)
-#         self.hass.data[DOMAIN][COORDINATORS].append(coordo)
-#         await coordo.async_refresh()
+        _LOGGER.debug(
+            "Adding Gree device %s at %s:%i",
+            device.device_info.name,
+            device.device_info.ip,
+            device.device_info.port,
+        )
+        coordo = DeviceDataUpdateCoordinator(self.hass, device)
+        self.hass.data[DOMAIN][COORDINATORS].append(coordo)
+        await coordo.async_refresh()
 
-#         async_dispatcher_send(self.hass, DISPATCH_DEVICE_DISCOVERED, coordo)
+        async_dispatcher_send(self.hass, DISPATCH_DEVICE_DISCOVERED, coordo)
 
-#     async def device_update(self, device_info: DeviceInfo) -> None:
-#         """Handle updates in device information, update if ip has changed."""
-#         for coordinator in self.hass.data[DOMAIN][COORDINATORS]:
-#             if coordinator.device.device_info.mac == device_info.mac:
-#                 coordinator.device.device_info.ip = device_info.ip
-#                 await coordinator.async_refresh()
+    async def device_update(self, device_info: DeviceInfo) -> None:
+        """Handle updates in device information, update if ip has changed."""
+        for coordinator in self.hass.data[DOMAIN][COORDINATORS]:
+            if coordinator.device.device_info.mac == device_info.mac:
+                coordinator.device.device_info.ip = device_info.ip
+                await coordinator.async_refresh()
